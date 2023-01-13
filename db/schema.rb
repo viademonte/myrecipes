8717617ef1330dadba5c4d42efc280352ad60a50
Diego Martinez Viademonte <viademonte@gmail.com>
2023-01-13 16:20:36 -0300

    Implement chef signup with tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230103211119) do
+ActiveRecord::Schema.define(version: 20230113145913) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   create_table "recipes", force: :cascade do |t|
